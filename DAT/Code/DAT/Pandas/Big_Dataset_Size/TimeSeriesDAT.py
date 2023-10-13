64f165b4bc4f311c098ff363d23c26ac5aa21e2d
karola65 <54160424+karola65@users.noreply.github.com>
2023-10-13 12:59:43 +0200

    test pouyeh

--- DAT/Code/DAT/Pandas/Big_Dataset_Size/TimeSeriesDAT.py
+++ DAT/Code/DAT/Pandas/Big_Dataset_Size/TimeSeriesDAT.py
@@ -1,7 +1,21 @@
 import pandas as pd
+import numpy as np
 
-# Generate a time series in Pandas
 start_date = "2023-01-01"
 end_date = "2033-01-10"
 date_range = pd.date_range(start=start_date, end=end_date)
-print(date_range)
+
+
+df = pd.read_csv('/Users/karolinabargiel/goGreen/DAT/Data/big_dataset.csv')
+
+print(len(df))
+
+# Generate random time series data
+np.random.seed(0)
+random_dates = np.random.choice(date_range, size=len(df))
+
+print(len(random_dates))
+
+
+# Add the random time series as a new column
+df['random_time_series'] = random_dates
--- DAT/Code/DAT/Polars/Big_Dataset_Size/TimeSeriesDAT.py
+++ DAT/Code/DAT/Polars/Big_Dataset_Size/TimeSeriesDAT.py
@@ -1,7 +1,17 @@
 import polars as pl
+import numpy as np
+
+# Load the DataFrame from the CSV file
+df = pl.read_csv('Data/big_dataset.csv')
 
 # Generate a time series in Polars
 start_date = "2023-01-01"
-end_date = "2033-01-10"
+end_date = "2023-01-10"
 date_range = pl.date_range(start=start_date, end=end_date)
-print(date_range)
+
+# Generate random time series data
+pl.seed(0)
+random_dates = date_range.sample(len(df), replace=True)
+
+# Add the random time series as a new column
+df = df.with_column(random_time_series=random_dates)
