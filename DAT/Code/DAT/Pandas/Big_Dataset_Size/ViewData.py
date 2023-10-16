3a5f1efaa29fe2d59422b15599dc1a6d84a0c42a
Apoorva NP <npapoorva1997@gmail.com>
2023-10-17 00:00:16 +0200

    revert changes

--- DAT/Code/DAT/Pandas/Big_Dataset_Size/ViewData.py
+++ DAT/Code/DAT/Pandas/Big_Dataset_Size/ViewData.py
@@ -1,23 +1,20 @@
 import pandas as pd
 
-i=0
-while(i<40):
-    # Create a sample DataFrame
-    df = pd.read_csv('DAT/Data/big_dataset.csv')
+# Create a sample DataFrame
+df = pd.read_csv('DAT/Data/big_dataset.csv')
 
-    # Display the DataFrame
-    print("Pandas: Display DataFrame using head()")
-    print(df.head())
+# Display the DataFrame
+print("Pandas: Display DataFrame using head()")
+print(df.head())
 
-    print("\nPandas: Display DataFrame using tail()")
-    print(df.tail())
+print("\nPandas: Display DataFrame using tail()")
+print(df.tail())
 
-    print("\nPandas: Display DataFrame dtypes")
-    print(df.dtypes)
+print("\nPandas: Display DataFrame dtypes")
+print(df.dtypes)
 
-    print("\nPandas: Display DataFrame describe()")
-    print(df.describe())
+print("\nPandas: Display DataFrame describe()")
+print(df.describe())
 
-    print("\nPandas: Display DataFrame shape")
-    print(df.shape)
-    i = i+1
+print("\nPandas: Display DataFrame shape")
+print(df.shape)
