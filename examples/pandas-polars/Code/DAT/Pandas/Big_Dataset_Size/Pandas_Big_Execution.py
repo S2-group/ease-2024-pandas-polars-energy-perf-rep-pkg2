3243fecc22dfa80df5f85e2044765c443b8d9c57
Felix <s0573580@htw-berlin.de>
2023-10-18 14:58:38 +0200

    BigSmallDatasetExecution stuff

--- examples/pandas-polars/Code/DAT/Pandas/Big_Dataset_Size/Pandas_Big_Execution.py
+++ examples/pandas-polars/Code/DAT/Pandas/Big_Dataset_Size/Pandas_Big_Execution.py
@@ -5,16 +5,6 @@ from StatisticalAggregationMinMaxUnique import StatisticalAggregationMinMaxUniqu
 from StatisticalAggregationSumMeanDAT import StatisticalAggregationSumMeanDAT
 from ViewData import ViewData
 
-file_names = [
-    'FileTypes.py',
-    'InputOutputDAT.py',
-    'MissingDataDAT.py',
-    'StatisticalAggregationMinMaxUnique.py',
-    'StatisticalAggregationSumMeanDAT.py',
-    'TimeSeriesDAT.py',
-    'ViewData.py'
-    ]
-
 def execute_files(n_times):
     for i in n_times:
         FileTypes()
