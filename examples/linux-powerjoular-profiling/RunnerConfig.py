a1a1ea209109babbacd4ea6ec8e0b0b42d0ebd01
Apoorva NP <npapoorva1997@gmail.com>
2023-10-18 10:38:43 +0200

    changed back to while loop true

--- examples/linux-powerjoular-profiling/RunnerConfig.py
+++ examples/linux-powerjoular-profiling/RunnerConfig.py
@@ -105,7 +105,7 @@ class RunnerConfig:
         # No interaction. We just run it for XX seconds.
         # Another example would be to wait for the target to finish, e.g. via `self.target.wait()`
         output.console_log("Running program for 20 seconds")
-        self.target.wait()
+        time.sleep(120)
 
     def stop_measurement(self, context: RunnerContext) -> None:
         """Perform any activity here required for stopping measurements."""
