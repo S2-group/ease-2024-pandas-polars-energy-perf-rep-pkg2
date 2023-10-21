6217f31e39bb1aa76cc1483346bee9533af73dbb
Apoorva NP <npapoorva1997@gmail.com>
2023-10-21 19:27:50 +0200

    Made bigger data set

--- examples/pandas-polars/Data/Build_Training_Set.py
+++ examples/pandas-polars/Data/Build_Training_Set.py
@@ -10,14 +10,17 @@ print(len(df))
 
 # Create a sub-dataset with 50,000 rows and save it to a CSV file
 sub_dataset = df.head(50000)
-sub_dataset.to_csv('final_small_dataset.csv', index=False)
+
+small_duplicated_datset = df.sample(n=4950000, replace=True)
+final_small_dataset = pd.concat([sub_dataset, small_duplicated_datset])
+final_small_dataset.to_csv('final_small_dataset.csv', index=False)
 
 # Duplicate the rows of the original dataset to create a second dataset with 2,000,000 rows
-duplicated_dataset = df.sample(n=1950000, replace=True)
+big_duplicated_dataset = df.sample(n=49500000, replace=True)
 # 1950000 is used because we already have 50000 rows in the sub_dataset
 
 # Concatenate the sub-dataset and the duplicated dataset
-final_dataset = pd.concat([sub_dataset, duplicated_dataset])
+final_big_dataset = pd.concat([sub_dataset, big_duplicated_dataset])
 
 # Save the final dataset to a CSV file
-final_dataset.to_csv('final_big_dataset.csv', index=False)
+final_big_dataset.to_csv('final_big_dataset.csv', index=False)
