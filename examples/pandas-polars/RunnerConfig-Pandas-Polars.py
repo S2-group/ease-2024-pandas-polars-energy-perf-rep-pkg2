86974fd9174f5801dd748eeb4ac018191acc9a14
Apoorva NP <npapoorva1997@gmail.com>
2023-10-22 17:35:43 +0200

    missing data and min max, unique

--- examples/pandas-polars/RunnerConfig-Pandas-Polars.py
+++ examples/pandas-polars/RunnerConfig-Pandas-Polars.py
@@ -81,7 +81,7 @@ class RunnerConfig:
         factor1 = FactorModel("run_number", ['r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'r10'])
         factor2 = FactorModel("library", ['Pandas', 'Polars'])
         factor3 = FactorModel("dataframe_size", ['Big', 'Small'])
-        factor4 = FactorModel("DAT", ['FileTypes', 'InputOutputDAT', 'MissingDataDAT', 'StatisticalAggregationMinMaxUnique', 'StatisticalAggregationSumMeanDAT', 'ViewData'])
+        factor4 = FactorModel("DAT", ['MissingDataDAT', 'StatisticalAggregationMinMaxUnique'])
         self.run_table_model = RunTableModel(
             factors=[factor1, factor2, factor3, factor4],
             data_columns=['execution_time', 'cpu_usage', 'memory_usage', 'energy_usage'],
@@ -125,7 +125,7 @@ class RunnerConfig:
                 file_name = "Polars_Small_Execution.py"
 
 
-        self.target = subprocess.Popen(['python3', f'examples/pandas-polars/Code/DAT/Polars/Big_Dataset_Size/{dat_filename}.py'])
+        self.target = subprocess.Popen(['python3', f'examples/pandas-polars/Code/DAT/Polars/Small_Dataset_Size/{dat_filename}.py'])
 
 
         print("hellooooooo I am here", self.target.pid)
