7c78d0a3deb771fbab8c2648a378f2edd91ee101
Apoorva NP <npapoorva1997@gmail.com>
2023-10-22 13:46:38 +0200

    Make dataset size 5 million and 500 thousand

--- examples/pandas-polars/Data/Build_Training_Set.py
+++ examples/pandas-polars/Data/Build_Training_Set.py
@@ -11,12 +11,12 @@ print(len(df))
 # Create a sub-dataset with 50,000 rows and save it to a CSV file
 sub_dataset = df.head(50000)
 
-small_duplicated_datset = df.sample(n=4950000, replace=True)
+small_duplicated_datset = df.sample(n=450000, replace=True)
 final_small_dataset = pd.concat([sub_dataset, small_duplicated_datset])
 final_small_dataset.to_csv('final_small_dataset.csv', index=False)
 
 # Duplicate the rows of the original dataset to create a second dataset with 2,000,000 rows
-big_duplicated_dataset = df.sample(n=49950000, replace=True)
+big_duplicated_dataset = df.sample(n=4950000, replace=True)
 # 1950000 is used because we already have 50000 rows in the sub_dataset
 
 # Concatenate the sub-dataset and the duplicated dataset
